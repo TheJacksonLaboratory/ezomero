c6e700cf9d3c41291c875f24bdc2ba591cf48642
Erick Martins Ratamero <erickmartins@gmx.net>
2021-01-13 10:36:02 -0500

    fixing import hierarchy

--- ezomero/ezomero.py
+++ ezomero/ezomero.py
@@ -6,6 +6,10 @@ from omero.model import DatasetImageLinkI, ImageI, ExperimenterI
 from omero.rtypes import rlong, rstring
 from omero.sys import Parameters
 
+#expose functions for import
+__all__ = ["post_dataset","post_image","post_map_annotation","post_project","get_image","get_image_ids","get_map_annotation_ids","get_map_annotation",
+           "get_group_id","get_user_id","get_original_filepaths","put_map_annotation","filter_by_filename","image_has_imported_filename",
+           "link_images_to_dataset","link_datasets_to_project","print_map_annotation","print_groups","print_projects","print_datasets","set_group"]
 
 # posts
 def post_dataset(conn, dataset_name, project_id=None, description=None):
