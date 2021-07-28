5f2fdcd199ae4070e7959a7c74c1e7f5180a6271
Dave Mellert <Dave.Mellert@jax.org>
2021-07-28 16:19:25 -0400

    set up for adding filters

--- ezomero/_gets.py
+++ ezomero/_gets.py
@@ -187,6 +187,8 @@ def get_image_ids(conn, project=None, dataset=None, plate=None, well=None,
         all images contained in all Wells belonging to the specified Plate.
     well : int, optional
         ID of Well from which to return image IDs.
+    filters : dict, optional
+        Refine query based on filters. See Notes below for implemented filters.
     across_groups : bool, optional
         Defines cross-group behavior of function - set to
         ``False`` to disable it.
@@ -215,10 +217,14 @@ def get_image_ids(conn, project=None, dataset=None, plate=None, well=None,
 
     >>> ds_ims = get_image_ids(conn, dataset=448)
     """
-    # Need to change this to check if more than one of
-    # Project, Dataset, Plate, Well are specified
-    if (dataset is not None) & (well is not None):
-        raise Exception('Dataset and Well can not both be specified')
+    # Need a test for this next chunk
+    arg_counter = 0
+    for arg in [project, dataset, plate, well]:
+        if arg is not None:
+            arg_counter += 1
+    if arg_counter > 1:
+        raise Exception('Only one of Project/Dataset/Plate/Well'
+                        ' can be specified')
 
     q = conn.getQueryService()
     params = Parameters()
@@ -294,98 +300,11 @@ def get_image_ids(conn, project=None, dataset=None, plate=None, well=None,
     else:
         results = []
 
-    return [r[0].val for r in results]
-
-
-@do_across_groups
-def get_image_ids_old(conn, dataset=None, well=None, across_groups=True):
-    """Return a list of image ids based on image container
-
-    If neither dataset nor well is specified, function will return orphans.
-
-    Parameters
-    ----------
-    conn : ``omero.gateway.BlitzGateway`` object
-        OMERO connection.
-    dataset : int, optional
-        ID of Dataset from which to return image IDs.
-    well : int, optional
-        ID of Well from which to return image IDs.
-    across_groups : bool, optional
-        Defines cross-group behavior of function - set to
-        ``False`` to disable it.
-
-    Returns
-    -------
-    im_ids : list of ints
-        List of image IDs contained in the given Dataset, Well, or orphans.
-
-    Notes
-    -----
-    User and group information comes from the `conn` object. Be sure to use
-    ``ezomero.set_group`` to specify group prior to passing
-    the `conn` object to this function.
-
-    If no Dataset or Well is specified, orphaned images are returned.
-
-    Examples
-    --------
-    # Return orphaned images:
-
-    >>> orphans = get_image_ids(conn)
-
-    # Return IDs of all images from Dataset with ID 448:
-
-    >>> ds_ims = get_image_ids(conn, dataset=448)
-    """
-    if (dataset is not None) & (well is not None):
-        raise Exception('Dataset and Well can not both be specified')
-
-    q = conn.getQueryService()
-    params = Parameters()
-
-    if dataset is not None:
-        if not isinstance(dataset, int):
-            raise TypeError('dataset must be integer')
-        params.map = {"dataset": rlong(dataset)}
-        results = q.projection(
-            "SELECT i.id FROM Dataset d"
-            " JOIN d.imageLinks dil"
-            " JOIN dil.child i"
-            " WHERE d.id=:dataset",
-            params,
-            conn.SERVICE_OPTS
-            )
-    elif well is not None:
-        if not isinstance(well, int):
-            raise TypeError('well must be integer')
-        params.map = {"well": rlong(well)}
-        results = q.projection(
-            "SELECT i.id FROM Well w"
-            " JOIN w.wellSamples ws"
-            " JOIN ws.image i"
-            " WHERE w.id=:well",
-            params,
-            conn.SERVICE_OPTS
-            )
-    elif (well is None) & (dataset is None):
-        results = q.projection(
-            "SELECT i.id FROM Image i"
-            " WHERE NOT EXISTS ("
-            " SELECT dil FROM DatasetImageLink dil"
-            " WHERE dil.child=i.id"
-            " )"
-            " AND NOT EXISTS ("
-            " SELECT ws from WellSample ws"
-            " WHERE ws.image=i.id"
-            " )",
-            params,
-            conn.SERVICE_OPTS
-            )
-    else:
-        results = []
-
-    return [r[0].val for r in results]
+    final_result = [r[0].val for r in results]
+    # Start filtering here!
+    if filters is not None:
+        pass
+    return final_result
 
 
 @do_across_groups
