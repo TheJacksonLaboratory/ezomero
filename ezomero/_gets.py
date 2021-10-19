49a034f03454fc6f54c41fa0699376e016e56a50
Erick Martins Ratamero <erickmartins@gmx.net>
2021-10-19 11:41:02 -0400

    appeasing the style gods

--- ezomero/_gets.py
+++ ezomero/_gets.py
@@ -552,8 +552,9 @@ def get_file_annotation(conn, file_ann_id, folder_path=None,
 
     Examples
     --------
-    >>> attch_path =
-            get_file_annotation(conn, 62, folder_path='/home/user/Downloads')
+    >>> attch_path = get_file_annotation(conn,
+                                         62,
+                                         folder_path='/home/user/Downloads')
     >>> print(attch_path)
     '/home/user/Downloads/attachment.txt'
     """
