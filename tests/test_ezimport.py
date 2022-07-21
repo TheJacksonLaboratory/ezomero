4af1a3db7e82273a1cdf13e91bbb99e75c581296
Erick Martins Ratamero <erickmartins@gmx.net>
2022-07-21 16:08:35 -0400

    Adding tests to `ezimport` (#57)
    * fixing imports with weird chars in path
    
    * moving tests to python 3.7
    
    * alright moving to 3.8 I guess
    
    * bumping omero-py to latest as well
    
    * Automated updating coverage badge
    
    * Automated updating coverage badge
    
    * mocking stdin to test ezimport
    
    * Automated updating coverage badge
    
    Co-authored-by: Erick Ratamero <erickmartins@users.noreply.github.com>

--- tests/test_ezimport.py
+++ tests/test_ezimport.py
@@ -1,62 +1,116 @@
-# import ezomero
-# import pytest
-
-
-# This file is a placeholder for the tests we would LIKE to run
-# (if running any tests that involve CLI was easier)
+import ezomero
+from io import StringIO
 
 # Test imports
 
 
-# def test_ezimport(self, conn):
+def test_ezimport(conn, monkeypatch):
 
-#     # test simple import, single file
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif")
-#     assert len(id) == 1
+    # test simple import, single file
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath)
+    assert len(id) == 1
+    conn.deleteObjects("Image", id)
 
 #     # test simple import, multifile/multi-image
-#     id = ezomero.ezimport(conn, "tests/data/vsi-ets-test-jpg2k.vsi")
-#     assert len(id) == 2
+    fpath = "tests/data/vsi-ets-test-jpg2k.vsi"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath)
+    assert len(id) == 2
 
 #     # test simple import, new orphan dataset
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif",
-#                             dataset="test_ds")
-#     assert len(id) == 1
-#     ds_id = ezomero.get_dataset_ids(conn)[-1]
-#     im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
-#     assert len(im_ids) == 1
-#     assert im_ids[0] == id
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath, dataset="test_ds")
+    assert len(id) == 1
+    ds_id = ezomero.get_dataset_ids(conn)[-1]
+    im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
+    assert len(im_ids) == 1
+    assert im_ids[0] == id[-1]
 
 #     # test simple import, existing dataset
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif",
-#                             dataset=ds_id)
-#     im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
-#     assert len(im_ids) == 2
-#     assert im_ids[-1] == id
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath, dataset=ds_id)
+    im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
+    assert len(im_ids) == 2
+    assert im_ids[-1] == id[-1]
+    conn.deleteObjects("Dataset", [ds_id], deleteChildren=True)
 
 #     # test simple import, new project
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif",
-#                             project="test_proj", dataset="test_ds")
-#     assert len(id) == 1
-#     proj_id = ezomero.get_project_ids(conn)[-1]
-#     ds_id = ezomero.get_dataset_ids(conn, project=proj_id)[-1]
-#     im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
-#     assert len(im_ids) == 1
-#     assert im_ids[0] == id
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath,
+                          project="test_proj", dataset="test_ds")
+    assert len(id) == 1
+    proj_id = ezomero.get_project_ids(conn)[-1]
+    ds_id = ezomero.get_dataset_ids(conn, project=proj_id)[-1]
+    im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
+    assert len(im_ids) == 1
+    assert im_ids[0] == id[-1]
 
 #     # test simple import, existing project, new dataset
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif",
-#                             project=proj_id, dataset="new_test_ds")
-#     ds_ids = ezomero.get_dataset_ids(conn, project=proj_id)
-#     im_ids = ezomero.get_image_ids(conn, dataset=ds_ids[-1])
-#     assert len(ds_ids) == 2
-#     assert len(im_ids) == 1
-#     assert im_ids[0] == id
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath,
+                          project=proj_id, dataset="new_test_ds")
+    ds_ids = ezomero.get_dataset_ids(conn, project=proj_id)
+    im_ids = ezomero.get_image_ids(conn, dataset=ds_ids[-1])
+    assert len(ds_ids) == 2
+    assert len(im_ids) == 1
+    assert im_ids[0] == id[-1]
 
 #     # test simple import, existing project, existing dataset
-#     id = ezomero.ezimport(conn, "tests/data/test_pyramid.ome.tif",
-#                             project=proj_id, dataset=ds_id)
-#     ds_id = ezomero.get_dataset_ids(conn, project=proj_id)[-1]
-#     im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
-#     assert len(im_ids) == 2
-#     assert im_ids[-1] == id
+    fpath = "tests/data/test_pyramid.ome.tif"
+    str_input = ["omero", 'import',
+                 '-k', conn.getSession().getUuid().val,
+                 '-s', conn.host,
+                 '-p', str(conn.port),
+                 fpath, "\n"]
+    io = StringIO(" ".join(str_input))
+    monkeypatch.setattr('sys.stdin', io)
+    id = ezomero.ezimport(conn, fpath,
+                          project=proj_id, dataset=ds_id)
+    ds_id = ezomero.get_dataset_ids(conn, project=proj_id)[0]
+    im_ids = ezomero.get_image_ids(conn, dataset=ds_id)
+    assert len(im_ids) == 2
+    assert im_ids[-1] == id[-1]
+
+    conn.deleteObjects("Project", [proj_id], deleteChildren=True)
